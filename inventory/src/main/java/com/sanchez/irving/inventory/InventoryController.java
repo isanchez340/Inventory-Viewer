d9c0e17bfb5dccccce4418b1a778e8ad873356fa
Irving Sanchez <sanchez.irving006@gmail.com>
2020-04-25 04:46:38 -0600

    fixes maybe????

--- inventory/src/main/java/com/sanchez/irving/inventory/InventoryController.java
+++ inventory/src/main/java/com/sanchez/irving/inventory/InventoryController.java
@@ -2,9 +2,11 @@ package com.sanchez.irving.inventory;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin(origins="https://isanchez340.github.io")
 @RestController
 public class InventoryController {
 	@Autowired
